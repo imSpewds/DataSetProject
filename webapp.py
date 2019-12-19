b43dd2bb45391831148afd9b099db5ef03c53384
BuildTools <unconfigured@null.spigotmc.org>
2019-12-18 19:52:59 -0800

    finished first page, made second page responsive, and many additions to python

--- webapp.py
+++ webapp.py
@@ -2,7 +2,35 @@ from flask import Flask, request, Markup, render_template, flash, url_for
 import os
 import json
 
-app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+app = Flask(__name__)
+
+with open('police_shootings.json') as shootings_data:
+    shootings = json.load(shootings_data)
+    
+"""Gets the victims names and if multiple of same name add a num ex.) TK TK(1), TK TK(2)..."""
+def get_victim_names():
+    listOfNames = []
+    for key in shootings:
+        listOfNames.append(key['Person']['Name'])
+    options = ""
+    for name in listOfNames:
+        options = options + Markup("<option value=\"" + name + "\">" + name + "</option>")
+    return options
+"""Gets the date of the incident"""
+def get_date(name):
+    date = shootings[0]['Incident']['Date']['Full']
+    for key in shootings:
+        if key['Person']['Name'] == name:
+            date = key['Incident']['Date']['Full']
+    return date
+"""Compiles the functions into one fact"""
+def get_shooting_facts():
+    name = request.args['name']
+    fact = ""
+    fact = fact + Markup(
+    "<h3>" + "ABOUT THE VICTIM" + "</h3>" + "<p>" + "Name: " + name + "<br>" + "Age: " + "AGE" + "<br>" +"Gender: " + "GENDER" + "<br>" +"Race: " + "RACE" + "<br>" + "</p>"
+    "<h3>" + "WHERE IT HAPPENED" + "</h3>" + "<p>" + "This victim was " + "MANNER" + " in " + "CITY" + " " + "STATE" + " on " + get_date(name) + "</p>")
+    return fact
 
 @app.route("/")
 def render_main():
@@ -10,11 +38,15 @@ def render_main():
     
 @app.route("/p2")
 def render_page2():
-    return render_template('page2.html')
+    return render_template('page2.html', options = get_victim_names())
+
+@app.route("/response")
+def render_response():
+    return render_template('page2.html', options = get_victim_names(), shootingFact = get_shooting_facts())
     
 @app.route("/p3")
 def render_page3():
     return render_template('page3.html')
 
 if __name__== "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
